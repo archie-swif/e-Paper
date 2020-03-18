d5b567a5b891a593a1231828c067e9849ee53819
Artem Riabokon <artem.riabokon@globallogic.com>
2020-03-17 17:15:28 -0700

    Added 8x8 symbols sample

--- RaspberryPi&JetsonNano/python/src/main.py
+++ RaspberryPi&JetsonNano/python/src/main.py
@@ -16,13 +16,22 @@ try:
 
     A = Symbols.get_symbol('A')
     X = Symbols.get_symbol('X')
+    NOT_A = Symbols.get_symbol('!A')
+
 
     display.print_symbol(A, 1, 0, display.black)
     display.print_symbol(X, 1, 1, display.black)
+    display.print_symbol(NOT_A, 1, 2, display.black)
+
     display.print_symbol(A, 2, 0, display.yellow)
     display.print_symbol(X, 2, 1, display.yellow)
+    display.print_symbol(NOT_A, 2, 2, display.yellow)
 
-    # display.yellow.ellipse((0, 0, 10, 10), fill=0)
+    display.black.rectangle((0, 24, 23, 31), fill=0)
+    display.print_symbol(A, 3, 0, display.yellow)
+    display.print_symbol(X, 3, 1, display.yellow)
+    display.print_symbol(A, 3, 2, display.black)
+    display.print_symbol(NOT_A, 3, 2, display.yellow)
 
     display.show_on_hardware()
     # display.show_on_software()
